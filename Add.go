e964aa3ed997a3a184d4c10fc6fd9ac0b5fcc540
kalyanThota1 <tpkalyan7@gmail.com>
2024-02-13 17:33:45 -0600

    ex3

--- Add.go
+++ Add.go
@@ -1,9 +1,15 @@
 package Add
 
+import "golang.org/x/exp/constraints"
+
 // Add function takes two integer parameters and return sum of two integers
 //
 // https://www.mathsisfun.com/numbers/addition.html
 
-func Add(v1, v2 int) int {
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+func Add[T Number](v1, v2 T) T {
 	return v1 + v2
 }
