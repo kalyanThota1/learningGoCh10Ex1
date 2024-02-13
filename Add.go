c0121ac14c8ea80bb48b5d5aa0061abb49e60b7b
kalyanThota1 <tpkalyan7@gmail.com>
2024-02-13 17:25:58 -0600

    defined function and added go doc comment

---
+++ Add.go
@@ -0,0 +1,9 @@
+package Add
+
+// Add function takes two integer parameters and return sum of two integers
+//
+// https://www.mathsisfun.com/numbers/addition.html
+
+func Add(v1, v2 int) int {
+	return v1 + v2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module ch10ex1
+
+go 1.21.6
